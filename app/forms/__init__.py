31678706c40d67161a5ab629b0006e3359a0be0d
Fabian Lopez <elopez@uno.edu.ar>
2026-07-30 15:10:43 -0300

    cambios

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, FileField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, SelectMultipleField, TextAreaField, FileField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, Optional
 
 
